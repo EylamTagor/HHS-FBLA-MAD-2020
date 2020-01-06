fa172479b09d86e9433916cef5337b558fcb6e57
Eylam Tagor <eylamtagor@gmail.com>
2020-01-05 17:38:13 -0800

    Finished reading data

--- app/src/main/java/com/hhsfbla/mad/MainActivity.java
+++ app/src/main/java/com/hhsfbla/mad/MainActivity.java
@@ -1,8 +1,8 @@
 package com.hhsfbla.mad;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
+
     }
 }
---
+++ app/src/main/java/com/hhsfbla/mad/Test.java
@@ -0,0 +1,21 @@
+package com.hhsfbla.mad;
+
+public class Test {
+    private int test;
+
+    public Test() {
+        test = 0;
+    }
+
+    public Test(int t) {
+        test = t;
+    }
+
+    public int getTest() {
+        return test;
+    }
+
+    public void setTest(int test) {
+        this.test = test;
+    }
+}
