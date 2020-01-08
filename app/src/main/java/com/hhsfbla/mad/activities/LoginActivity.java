13b42a2e38a6e43738a6f4fdbb4675f303f97dae
Radhika <ragarwal865@student.fuhsd.org>
2020-01-07 20:51:14 -0800

    finished log in page

--- app/src/main/java/com/hhsfbla/mad/activities/LoginActivity.java
+++ app/src/main/java/com/hhsfbla/mad/activities/LoginActivity.java
@@ -3,14 +3,27 @@ package com.hhsfbla.mad.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.hhsfbla.mad.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView welcomeTxtView;
+    private Button loginGoogleBtn;
+    private Button loginFacebookBtn;
+    private Button loginBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        welcomeTxtView = findViewById(R.id.welcomeTxtView);
+        loginGoogleBtn = findViewById(R.id.loginGoogleBtn);
+        loginFacebookBtn = findViewById(R.id.loginFacebookBtn);
+        loginBtn = findViewById(R.id.loginBtn);
+
     }
 }
--- app/src/main/java/com/hhsfbla/mad/activities/MainActivity.java
+++ app/src/main/java/com/hhsfbla/mad/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.hhsfbla.mad.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +11,14 @@ import com.hhsfbla.mad.R;
 public class MainActivity extends AppCompatActivity {
     // login/signup (welcome)
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
