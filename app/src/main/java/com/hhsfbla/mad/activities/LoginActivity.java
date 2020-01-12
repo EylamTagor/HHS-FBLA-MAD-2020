752467375425199cfa7e877432ce852df2f1c083
Arnav Kulkarni <arnavyk@gmail.com>
2020-01-11 16:00:52 -0800

    added transition to homepage from facebook button

--- app/src/main/java/com/hhsfbla/mad/activities/LoginActivity.java
+++ app/src/main/java/com/hhsfbla/mad/activities/LoginActivity.java
@@ -130,7 +130,8 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
 //                            Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
+                            user = FirebaseAuth.getInstance().getCurrentUser();
+                            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
                         } else {
                             // If sign in fails, display a message to the user.
 //                            Log.w(TAG, "signInWithCredential:failure", task.getException());
