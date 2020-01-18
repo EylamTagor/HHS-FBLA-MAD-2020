166a5baba375c691c061a26e82e625da104410d1
Arnav Kulkarni <arnavyk@gmail.com>
2020-01-18 15:41:07 -0800

    added profile page

--- app/src/main/java/com/hhsfbla/mad/activities/ProfileActivity.java
+++ app/src/main/java/com/hhsfbla/mad/activities/ProfileActivity.java
@@ -2,15 +2,39 @@ package com.hhsfbla.mad.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.hhsfbla.mad.R;
 
+import java.io.InputStream;
+import java.net.URL;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseUser user;
+    private ImageView profilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        profilePic = findViewById(R.id.profilePic);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        profilePic.setImageDrawable(LoadImageFromWebOperations(user.getPhotoUrl().toString()));
+    }
+
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
