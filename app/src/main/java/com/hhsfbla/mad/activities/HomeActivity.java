31f2dca85048d973240fdffbebcd5113e84fd1dc
Radhika <ragarwal865@student.fuhsd.org>
2020-01-18 16:02:28 -0800

    drawer complete

--- app/src/main/java/com/hhsfbla/mad/activities/HomeActivity.java
+++ app/src/main/java/com/hhsfbla/mad/activities/HomeActivity.java
@@ -57,8 +57,8 @@ public class HomeActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_home, R.id.nav_about, R.id.nav_about_chapter,
+                R.id.nav_calendar, R.id.nav_contact, R.id.nav_comps, R.id.nav_my_events, R.id.nav_officer)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
