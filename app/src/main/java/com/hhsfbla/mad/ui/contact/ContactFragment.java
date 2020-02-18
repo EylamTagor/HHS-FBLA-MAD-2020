da6ca1f2ffef17481709008447f40b6d01aea8fe
Radhika <ragarwal865@student.fuhsd.org>
2020-02-17 22:43:37 -0800

    contact us almost done

--- app/src/main/java/com/hhsfbla/mad/ui/contact/ContactFragment.java
+++ app/src/main/java/com/hhsfbla/mad/ui/contact/ContactFragment.java
@@ -1,22 +1,34 @@
 package com.hhsfbla.mad.ui.contact;
 
+import androidx.annotation.ColorRes;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.hhsfbla.mad.R;
 
 public class ContactFragment extends Fragment {
 
     private ContactViewModel mViewModel;
+    TextView fblaLink;
+    ImageButton facebook, insta, twitter, linkedin;
+
+
 
     public static ContactFragment newInstance() {
         return new ContactFragment();
@@ -25,7 +37,49 @@ public class ContactFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        fblaLink = root.findViewById(R.id.fblaWebLinkTxtView);
+        fblaLink.setText(Html.fromHtml("<a href='https://www.fbla-pbl.org'>FBLA Website Link</androidx.constraintlayout.widget.ConstraintLayout<a>"));
+        fblaLink.setMovementMethod(LinkMovementMethod.getInstance());
+        fblaLink.setTextColor(Color.BLACK);
+
+        facebook = root.findViewById(R.id.facebookButton);
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/FutureBusinessLeaders")));
+            }
+        });
+
+        insta = root.findViewById(R.id.instaButton);
+        insta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/fbla_pbl/")));
+            }
+        });
+
+        twitter = root.findViewById(R.id.twitterButton);
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/FBLA_National")));
+            }
+        });
+
+
+        linkedin = root.findViewById(R.id.linkedinButton);
+        linkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/company/future-business-leaders-america-phi-beta-lambda/")));
+            }
+        });
+
+        return root;
+
     }
 
     @Override
