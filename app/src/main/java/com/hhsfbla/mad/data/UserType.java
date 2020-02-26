8d3d82e2b7e3699a65974be1dd1a1b7422db2897
Arnav Kulkarni <arnavyk@gmail.com>
2020-02-25 23:44:09 -0800

    data model javadoc finished

--- app/src/main/java/com/hhsfbla/mad/data/UserType.java
+++ app/src/main/java/com/hhsfbla/mad/data/UserType.java
@@ -1,16 +1,30 @@
 package com.hhsfbla.mad.data;
 
+/**
+ * Represents all the different types a user can be
+ */
 public enum UserType {
 
+    /**
+     * Member, lowest level
+     */
     MEMBER {
         public String toString() {
             return "Member";
         }
-    }, OFFICER {
+    },
+    /**
+     * Officer: added ability to create and manage events
+     */
+    OFFICER {
         public String toString() {
             return "Officer";
         }
-    }, ADVISOR {
+    },
+    /**
+     * Advisor: added ability to create and manage events as well as members and officers of the chapter
+     */
+    ADVISOR {
         public String toString() {
             return "Advisor";
         }
